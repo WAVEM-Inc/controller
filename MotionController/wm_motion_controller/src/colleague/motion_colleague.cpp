2f5b67de4a301b70ebbc3c7cc9446878a3b751ee
nuc-bt <felicita64@gmail.com>
2024-04-15 13:06:01 +0900

    chore : 안쓰는 코드 제거

--- MotionController/wm_motion_controller/src/colleague/motion_colleague.cpp
+++
@@ -1,6 +0,0 @@
-#include "colleague/i_motion_colleague.hpp"
-/*
-IMotionColleague::IMotionColleague(std::shared_ptr<IMotionMediator> motion_mediator) : m_i_motion_mediator(motion_mediator){   
-    
-}
-*/
